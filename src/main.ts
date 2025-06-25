e94b8402cebc5dba3ab179940562c3ecc5510cb8
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2025-06-25 22:23:49 +0300

    fix(app): корректная инициализация Naive UI до mount

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import {createApp} from 'vue'
-import App from "./App.vue"
+import App from './App.vue'
 import {createNaiveUI} from './plugins/naive'
 import {initThemeWatcher} from './app/theme'
-import { router } from '@/app/router'
+import {router} from '@/app/router'
 
 initThemeWatcher()
 
-const app = createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
 app.use(createNaiveUI())
+
 app.mount('#app')
